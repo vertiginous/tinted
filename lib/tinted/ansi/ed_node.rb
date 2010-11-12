2e7526d11b520776854551f0458c4f3740266799
Gordon Thiesfeld <gthiesfeld@gmail.com>
2010-11-12 09:29:04 -0600

    add more ANSI codes to parser nodes

--- lib/tinted/ansi/ed_node.rb
+++ lib/tinted/ansi/ed_node.rb
@@ -2,8 +2,14 @@ module Tinted
   module ANSI
     class EDNode < Treetop::Runtime::SyntaxNode
 
+      CODES = {
+                0 => { :clear => :end },
+                1 => { :clear => :beginning },
+                2 => { :clear => :all },
+              }
+
       def output
-        code.text_value
+        CODES[code.text_value]
       end
       
     end
